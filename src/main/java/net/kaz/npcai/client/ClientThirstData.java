4196c9fbf3232ca7d0ee7d303e76f3794d18d243
kazenoko-git <kazenoko.main@gmail.com>
2023-03-01 00:04:23 +0530

    added networking, player capability (thirst)

---
+++ src/main/java/net/kaz/npcai/client/ClientThirstData.java
@@ -0,0 +1,12 @@
+package net.kaz.npcai.client;
+
+public class ClientThirstData {
+    private static int playerThirst;
+
+    public static void set(int thirst) {
+        ClientThirstData.playerThirst = thirst;
+    }
+
+    public static int getPlayerThirst() {
+        return playerThirst;
+}}
